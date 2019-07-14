9491a3293d14ef66e3ae00bc837520af0d0ecebc
Okorie <Simon.Okorie@shell.com>
2019-07-14 16:05:11 +0100

    Updates

---
+++ precache-manifest.01363a1b2476f93746a79328a34cb1db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afb51e28f973d9845f251fc48a3c058",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "83490147f6860137bd9b",
+    "url": "/monsters-rolodex/static/css/main.051ac4f8.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "83490147f6860137bd9b",
+    "url": "/monsters-rolodex/static/js/main.10faf161.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
